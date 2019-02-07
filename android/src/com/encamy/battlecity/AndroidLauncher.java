99dd669ef77da57a4d07048e003c40ec016e22eb
Aleksei <alexxey67@gmail.com>
2019-02-07 21:50:18 +0300

    Initial commit

---
+++ android/src/com/encamy/battlecity/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.encamy.battlecity;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.encamy.battlecity.BattleCity;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BattleCity(), config);
+	}
+}
---
+++ desktop/src/com/encamy/battlecity/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.encamy.battlecity.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.encamy.battlecity.BattleCity;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BattleCity(), config);
+	}
+}
