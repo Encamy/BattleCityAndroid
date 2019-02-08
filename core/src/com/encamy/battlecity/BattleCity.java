4fa7e19d7fcf7f19a4f7b8196215c6d0f5c63c52
Aleksei <alexxey67@gmail.com>
2019-02-08 23:57:45 +0300

    Add Player. Add animation. Rework tiles

--- core/src/com/encamy/battlecity/BattleCity.java
+++ core/src/com/encamy/battlecity/BattleCity.java
@@ -4,16 +4,32 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.objects.TextureMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapImageLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+import java.util.Iterator;
+
+import sun.rmi.runtime.Log;
 
 public class BattleCity extends ApplicationAdapter
 {
 	//SpriteBatch batch;
-    OrthographicCamera m_camera;
-    TiledMap m_tileMap;
-    OrthogonalTiledMapRenderer m_renderer;
+	private OrthographicCamera m_camera;
+	private TiledMap m_tileMap;
+    private OrthogonalTiledMapRenderer m_renderer;
+
+    private Player m_player;
+
+	private static final float FRAME_DURATION = 0.20f;
 
 	@Override
 	public void create ()
@@ -26,7 +42,41 @@ public class BattleCity extends ApplicationAdapter
         m_camera.viewportHeight = 540;
 
 		m_tileMap = new TmxMapLoader().load("general_map.tmx");
-		m_renderer = new OrthogonalTiledMapRenderer(m_tileMap, 0.5f);
+		m_renderer = new OrthogonalTiledMapRenderer(m_tileMap, 0.8f);
+
+		TextureAtlas atlas = new TextureAtlas();
+
+		Iterator<TiledMapTile> tiles = m_tileMap.getTileSets().getTileSet("entities").iterator();
+		while (tiles.hasNext())
+		{
+			TiledMapTile tile = tiles.next();
+			if (tile.getProperties().containsKey("tank_name") && tile.getProperties().get("tank_name", String.class).equals("yellow"))
+			{
+				if (tile.getProperties().containsKey("tank_type") && tile.getProperties().get("tank_type", String.class).equals("1"))
+				{
+					if (!tile.getProperties().containsKey("direction"))
+					{
+						Gdx.app.log("ERROR", "Invalid tile format");
+					}
+
+					atlas.addRegion(tile.getProperties().get("direction", String.class), tile.getTextureRegion());
+				}
+			}
+		}
+
+		Animation left = new Animation(FRAME_DURATION, atlas.findRegions("left"));
+		Animation top = new Animation(FRAME_DURATION, atlas.findRegions("top"));
+		Animation right = new Animation(FRAME_DURATION, atlas.findRegions("right"));
+		Animation bottom = new Animation(FRAME_DURATION, atlas.findRegions("bottom"));
+		left.setPlayMode(Animation.PlayMode.LOOP);
+		top.setPlayMode(Animation.PlayMode.LOOP);
+		right.setPlayMode(Animation.PlayMode.LOOP);
+		bottom.setPlayMode(Animation.PlayMode.LOOP);
+
+		m_player = new Player(left, top, right, bottom);
+		m_player.setPosition(0,0);
+
+		Gdx.input.setInputProcessor(m_player);
 	}
 
 	@Override
@@ -35,8 +85,13 @@ public class BattleCity extends ApplicationAdapter
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        //m_camera.update();
-        m_renderer.setView(m_camera);
+		m_renderer.setView(m_camera);
+
+		//m_camera.update();
+		m_renderer.getBatch().begin();
+		m_player.draw(m_renderer.getBatch());
+		m_renderer.getBatch().end();
+
         m_renderer.render();
 
         //batch.setProjectionMatrix(m_camera.combined);
---
+++ core/src/com/encamy/battlecity/Player.java
@@ -0,0 +1,137 @@
+package com.encamy.battlecity;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
+
+class Player extends Sprite implements InputProcessor {
+
+    private Vector2 velocity = new Vector2();
+    private float speed = 60 * 2;
+    private float m_animationTime = 0;
+    private Animation m_left, m_top, m_right, m_bottom;
+
+    public Player(Animation left, Animation top, Animation right, Animation bottom)
+    {
+        super(((TextureAtlas.AtlasRegion) top.getKeyFrame(0)));
+        m_left = left;
+        m_top = top;
+        m_right = right;
+        m_bottom = bottom;
+    }
+
+    @Override
+    public void draw(Batch batch) {
+        update(Gdx.graphics.getDeltaTime());
+        super.draw(batch);
+    }
+
+    private void update(float deltaTime) {
+        // clamp velocity
+        /*if (velocity.y > speed)
+        {
+            velocity.y = speed;
+        }
+        else if (velocity.y < speed)
+        {
+            velocity.y = -speed;
+        }
+*/
+        // update position
+        setX(getX() + velocity.x * deltaTime);
+        setY(getY() + velocity.y * deltaTime);
+
+        // update animation
+        m_animationTime += deltaTime;
+        if (velocity.x < 0)
+        {
+            super.setRegion(((TextureAtlas.AtlasRegion) m_left.getKeyFrame(m_animationTime)));
+        }
+        else if (velocity.x > 0)
+        {
+            super.setRegion(((TextureAtlas.AtlasRegion) m_right.getKeyFrame(m_animationTime)));
+        }
+        else if (velocity.y < 0)
+        {
+            super.setRegion(((TextureAtlas.AtlasRegion) m_bottom.getKeyFrame(m_animationTime)));
+        }
+        else if (velocity.y > 0)
+        {
+            super.setRegion(((TextureAtlas.AtlasRegion) m_top.getKeyFrame(m_animationTime)));
+        }
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        switch (keycode)
+        {
+            case Input.Keys.W:
+                velocity.y = speed;
+                break;
+            case Input.Keys.A:
+                velocity.x = -speed;
+                break;
+            case Input.Keys.D:
+                velocity.x = speed;
+                break;
+            case Input.Keys.S:
+                velocity.y = -speed;
+                break;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        switch (keycode)
+        {
+            case Input.Keys.W:
+            case Input.Keys.S:
+                velocity.y = 0;
+                break;
+            case Input.Keys.A:
+            case Input.Keys.D:
+                velocity.x = 0;
+                break;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
