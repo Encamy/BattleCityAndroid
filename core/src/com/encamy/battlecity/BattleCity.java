f937b65ac1084d1ccc15c2ce61a7234e013ba872
Aleksei <alexxey67@gmail.com>
2019-02-10 20:06:45 +0300

    Fully rework collisions. Use Box2d as physics engine

--- core/src/com/encamy/battlecity/BattleCity.java
+++ core/src/com/encamy/battlecity/BattleCity.java
@@ -2,6 +2,8 @@ package com.encamy.battlecity;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Animation;
@@ -13,20 +15,30 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Shape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.encamy.battlecity.entities.EnemyFactory;
 import com.encamy.battlecity.entities.Player;
 
 import java.util.Iterator;
+import java.util.Set;
 
 import static com.encamy.battlecity.Settings.ANIMATION_FRAME_DURATION;
 import static com.encamy.battlecity.Settings.APPLICATION_VERSION;
 import static com.encamy.battlecity.Settings.SCREEN_HEIGHT;
 import static com.encamy.battlecity.Settings.SCREEN_WIDTH;
 
-public class BattleCity extends ApplicationAdapter
-{
+public class BattleCity extends ApplicationAdapter implements InputProcessor {
 	//SpriteBatch batch;
 	private OrthographicCamera m_camera;
 	private Viewport m_viewport;
@@ -34,7 +46,11 @@ public class BattleCity extends ApplicationAdapter
     private OrthogonalTiledMapRenderer m_renderer;
 
     private Player m_player;
+    private Body m_playerBody;
     private EnemyFactory m_enemyFactory;
+
+    private Box2DDebugRenderer m_b2drenderer;
+    private World m_world;
 
 	@Override
 	public void create ()
@@ -48,6 +64,10 @@ public class BattleCity extends ApplicationAdapter
 		m_tileMap = new TmxMapLoader().load("general_map.tmx");
 		m_renderer = new OrthogonalTiledMapRenderer(m_tileMap);
         m_renderer.setView(m_camera);
+
+        Box2D.init();
+        m_b2drenderer = new Box2DDebugRenderer();
+        m_world = new World(new Vector2(0,0), true);
 
         Gdx.app.log("Trace", "Loading animations");
 
@@ -79,16 +99,79 @@ public class BattleCity extends ApplicationAdapter
         Gdx.app.log("Trace", "Loading collisions layer");
 
 		MapObjects walls  = m_tileMap.getLayers().get("Collisions").getObjects();
-		
-		m_player = new Player(left, top, right, bottom, walls);
-		m_player.setPosition(spawnpoint.x, spawnpoint.y);
 
+        for (MapObject object : walls)
+        {
+            if (object instanceof RectangleMapObject)
+            {
+                Rectangle rectangle = ((RectangleMapObject) object).getRectangle();
+
+                createBox(
+                        m_world,
+                        rectangle.x,
+                        rectangle.y,
+                        rectangle.width,
+                        rectangle.height,
+                        true);
+            }
+        }
+
+       m_playerBody = createPlayerBox(
+                m_world,
+                spawnpoint.x,
+                spawnpoint.y,
+                54, 54);
+
+        m_player = new Player(left, top, right, bottom, m_playerBody);
         m_enemyFactory = new EnemyFactory(m_tileMap.getLayers().get("EnemySpawns").getObjects(), atlas);
 
-		Gdx.input.setInputProcessor(m_player);
+		Gdx.input.setInputProcessor(this);
 	}
 
-	@Override
+    private Body createBox(World world, float x, float y, float w, float h, boolean isStatic)
+    {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.fixedRotation = true;
+        bodyDef.linearDamping = 10f;
+
+        if (isStatic)
+        {
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+        }
+        else
+        {
+            bodyDef.type = BodyDef.BodyType.DynamicBody;
+        }
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(w * 0.5f, h * 0.5f, new Vector2(x + w * 0.5f - Settings.SCREEN_WIDTH * 0.5f, y + h * 0.5f - Settings.SCREEN_HEIGHT * 0.5f), 0.0f);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 1.0f;
+
+        return world.createBody(bodyDef).createFixture(fixtureDef).getBody();
+    }
+
+    private Body createPlayerBox(World world, float x, float y, float w, float h)
+    {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.fixedRotation = true;
+        bodyDef.linearDamping = 10f;
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(x - SCREEN_WIDTH * 0.5f + 32, y - SCREEN_HEIGHT * 0.5f + 32);
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(w * 0.5f, h * 0.5f);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 1.0f;
+
+        return world.createBody(bodyDef).createFixture(fixtureDef).getBody();
+    }
+
+    @Override
 	public void render ()
     {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
@@ -103,7 +186,10 @@ public class BattleCity extends ApplicationAdapter
 		m_renderer.getBatch().end();
 
         m_renderer.render();
-	}
+
+        m_world.step(1/60f, 6, 2);
+        m_b2drenderer.render(m_world, m_camera.projection);
+    }
 	
 	@Override
 	public void dispose ()
@@ -144,4 +230,100 @@ public class BattleCity extends ApplicationAdapter
 
 		return atlas;
 	}
+
+    @Override
+    public boolean keyDown(int keycode)
+    {
+        switch (keycode)
+        {
+            case Input.Keys.W:
+                m_player.setVelocity(0.0f, m_player.getSpeed());
+                break;
+            case Input.Keys.A:
+                m_player.setVelocity(-1 * m_player.getSpeed(), 0.0f);
+                break;
+            case Input.Keys.D:
+                m_player.setVelocity(m_player.getSpeed(), 0.0f);
+                break;
+            case Input.Keys.S:
+                m_player.setVelocity(0.0f, -1 * m_player.getSpeed());
+                break;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int keycode)
+    {
+        m_player.setVelocity(0.0f, 0.0f);
+
+        return true;
+    }
+
+    @Override
+    public boolean keyTyped(char character)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button)
+    {
+        m_player.setVelocity(0.0f, 0.0f);
+        return true;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer)
+    {
+        if (screenX > Gdx.graphics.getWidth() * 0.3f)
+        {
+            return false;
+        }
+
+        if (screenY < Gdx.graphics.getHeight() * 0.3f)
+        {
+            m_player.setVelocity(0, m_player.getSpeed());
+            return  true;
+        }
+
+        if (screenY > Gdx.graphics.getHeight() * 0.6f)
+        {
+            m_player.setVelocity(0, -1 * m_player.getSpeed());
+            return  true;
+        }
+
+        if (screenX < Gdx.graphics.getWidth() * 0.15f)
+        {
+            m_player.setVelocity(-1 * m_player.getSpeed(), 0.0f);
+            return  true;
+        }
+
+        if (screenX > Gdx.graphics.getWidth() * 0.15f)
+        {
+            m_player.setVelocity(m_player.getSpeed(), 0.0f);
+            return  true;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount)
+    {
+        return false;
+    }
 }
--- core/src/com/encamy/battlecity/entities/Player.java
+++ core/src/com/encamy/battlecity/entities/Player.java
@@ -15,18 +15,20 @@ import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Shape;
 import com.encamy.battlecity.Settings;
 
 
-public class Player extends Sprite implements InputProcessor {
+public class Player extends Sprite {
 
     private Vector2 velocity = new Vector2();
-    private float speed = Settings.BASE_MOVEMENT_SPEED;
+    private float m_speed = Settings.BASE_MOVEMENT_SPEED;
     private float m_animationTime = 0;
     private Animation m_left, m_top, m_right, m_bottom;
-    private MapObjects m_walls;
+    private Body m_body;
 
-    public Player(Animation left, Animation top, Animation right, Animation bottom, MapObjects walls)
+    public Player(Animation left, Animation top, Animation right, Animation bottom, Body body)
     {
         super(((TextureAtlas.AtlasRegion) top.getKeyFrame(0)));
         m_left = left;
@@ -34,7 +36,9 @@ public class Player extends Sprite implements InputProcessor {
         m_right = right;
         m_bottom = bottom;
 
-        m_walls = walls;
+        m_body = body;
+       // setX(body.getPosition().x);
+      //  setY(body.getPosition().y);
     }
 
     @Override
@@ -42,6 +46,16 @@ public class Player extends Sprite implements InputProcessor {
     {
         update(Gdx.graphics.getDeltaTime());
         super.draw(batch);
+    }
+
+    public void setVelocity(float x, float y)
+    {
+        velocity.set(x, y);
+    }
+
+    public float getSpeed()
+    {
+        return m_speed;
     }
 
     private void update(float deltaTime)
@@ -65,160 +79,11 @@ public class Player extends Sprite implements InputProcessor {
             super.setRegion(((TextureAtlas.AtlasRegion) m_top.getKeyFrame(m_animationTime)));
         }
 
-        if (!canMove(getX() + velocity.x * deltaTime, getY() + velocity.y * deltaTime))
-        {
-            return;
-        }
+        m_body.setLinearVelocity(velocity.x, velocity.y);
 
-        // update position
-        setX(getX() + velocity.x * deltaTime);
-        setY(getY() + velocity.y * deltaTime);
-    }
 
-    private boolean canMove(float x, float y)
-    {
-        Rectangle player = new Rectangle();
-
-        // Some player-friendly assumptions
-        player.x = x + 4;
-        player.y = y + 4;
-        player.width = 52;
-        player.height = 52;
-
-        if (player.x < 0 || player.y < 0 || player.x + player.width > Settings.SCREEN_WIDTH || player.y + player.height > Settings.SCREEN_HEIGHT)
-        {
-            return false;
-        }
-
-        for (MapObject object : m_walls)
-        {
-            if (object instanceof RectangleMapObject)
-            {
-                Rectangle rectangle = ((RectangleMapObject) object).getRectangle();
-
-                if (Intersector.overlaps(rectangle, player))
-                {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
-    @Override
-    public boolean keyDown(int keycode)
-    {
-        switch (keycode)
-        {
-            case Input.Keys.W:
-                velocity.y = speed;
-                velocity.x = 0;
-                break;
-            case Input.Keys.A:
-                velocity.x = -speed;
-                velocity.y = 0;
-                break;
-            case Input.Keys.D:
-                velocity.x = speed;
-                velocity.y = 0;
-                break;
-            case Input.Keys.S:
-                velocity.y = -speed;
-                velocity.x = 0;
-                break;
-        }
-
-        return true;
-    }
-
-    @Override
-    public boolean keyUp(int keycode)
-    {
-        switch (keycode)
-        {
-            case Input.Keys.W:
-            case Input.Keys.S:
-                velocity.y = 0;
-                break;
-            case Input.Keys.A:
-            case Input.Keys.D:
-                velocity.x = 0;
-                break;
-        }
-
-        return true;
-    }
-
-    @Override
-    public boolean keyTyped(char character)
-    {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button)
-    {
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button)
-    {
-        velocity.x = 0;
-        velocity.y = 0;
-
-        return true;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer)
-    {
-        if (screenX > Gdx.graphics.getWidth() * 0.3f)
-        {
-            return false;
-        }
-
-        if (screenY < Gdx.graphics.getHeight() * 0.3f)
-        {
-            velocity.y = speed;
-            velocity.x = 0;
-            return  true;
-        }
-
-        if (screenY > Gdx.graphics.getHeight() * 0.6f)
-        {
-            velocity.y = -speed;
-            velocity.x = 0;
-            return  true;
-        }
-
-        if (screenX < Gdx.graphics.getWidth() * 0.15f)
-        {
-            velocity.x = -speed;
-            velocity.y = 0;
-            return  true;
-        }
-
-        if (screenX > Gdx.graphics.getWidth() * 0.15f)
-        {
-            velocity.x = speed;
-            velocity.y = 0;
-            return  true;
-        }
-
-        return true;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY)
-    {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(int amount)
-    {
-        return false;
+        Gdx.app.log("Trace", "Current player position: " + getX() + ":" + getY());
+        setX(m_body.getPosition().x + Settings.SCREEN_WIDTH * 0.5f - 32);
+        setY(m_body.getPosition().y + Settings.SCREEN_HEIGHT * 0.5f - 32);
     }
 }
