651b0f04ec7f18102f2b69a2fb855767d482e975
Aleksei <alexxey67@gmail.com>
2019-02-10 16:04:21 +0300

    Resolve spawn intersection

--- core/src/com/encamy/battlecity/EnemyFactory.java
+++ core/src/com/encamy/battlecity/EnemyFactory.java
@@ -6,8 +6,14 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
+import org.w3c.dom.css.Rect;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Vector;
 
@@ -69,6 +75,12 @@ public class EnemyFactory
     {
         Gdx.app.log("Trace", "Spawning enemy");
         Vector2 spawnpoint = getSpawnPoint();
+
+        if (spawnpoint == null)
+        {
+            return;
+        }
+
         PlaySpawnAnimation(spawnpoint);
 
         m_enemies.add(CreateRandomEnemy(spawnpoint));
@@ -79,13 +91,45 @@ public class EnemyFactory
         // just a placeholder for now
     }
 
-    private Vector2 getSpawnPoint()
-    {
-        MapObject spawnObject = m_spawnPoints.get(m_random.nextInt(m_spawnPoints.getCount()));
-        Vector2 spawnpoint = new Vector2(
-                ((RectangleMapObject)spawnObject).getRectangle().getX(),
-                ((RectangleMapObject)spawnObject).getRectangle().getY()
-        );
+    private Vector2 getSpawnPoint() {
+        List<Vector2> availableSpawns = new ArrayList<Vector2>();
+
+        for (MapObject object : m_spawnPoints) {
+            if (object instanceof RectangleMapObject) {
+                Rectangle rectangle = ((RectangleMapObject) object).getRectangle();
+                rectangle.width = 64;
+                rectangle.height = 64;
+
+                boolean intersect = false;
+
+                for (Enemy enemy : m_enemies) {
+                    Rectangle enemyRectangle = new Rectangle();
+
+                    enemyRectangle.x = enemy.getX();
+                    enemyRectangle.y = enemy.getY();
+                    enemyRectangle.width = 64;
+                    enemyRectangle.height = 64;
+
+                    if (Intersector.overlaps(rectangle, enemyRectangle)) {
+                        intersect = true;
+                        break;
+                    }
+                }
+
+                if (intersect == false) {
+                    availableSpawns.add(new Vector2(rectangle.getX(), rectangle.getY()));
+                }
+            }
+        }
+
+        if (availableSpawns.size() == 0)
+        {
+            Gdx.app.log("Trace", "There is no available spawns");
+            return null;
+        }
+
+        Vector2 spawnpoint = availableSpawns.get(m_random.nextInt(availableSpawns.size()));
+        Gdx.app.log("Trace", "Choosing spawnpoint = " + spawnpoint.x + ":" + spawnpoint.y);
 
         return spawnpoint;
     }
