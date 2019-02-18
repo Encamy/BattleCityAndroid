3706aca0a8b42690b99b5d57924c9caf3fbeaca0
Aleksei <alexxey67@gmail.com>
2019-02-18 21:57:38 +0300

    Bump version

--- core/src/com/encamy/battlecity/Settings.java
+++ core/src/com/encamy/battlecity/Settings.java
@@ -1,7 +1,7 @@
 package com.encamy.battlecity;
 
 public class Settings {
-    public static String APPLICATION_VERSION = "0.1";
+    public static String APPLICATION_VERSION = "0.2";
     public static int SCREEN_WIDTH = 1280;
     public static int SCREEN_HEIGHT = 720;
     public static final float ANIMATION_FRAME_DURATION = 0.20f;
